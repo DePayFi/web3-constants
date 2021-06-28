644e7ee45854da48d003e4c979f3cac5e957ae02
Sebastian Pape <s@pape.io>
2021-06-28 16:02:42 +0200

    constant for WRAPPED token addresses

--- tests/units/ethereum.spec.js
+++ tests/units/ethereum.spec.js
@@ -6,6 +6,7 @@ describe('ethereum', () => {
 
     expect(CONSTANTS['ethereum'].ZERO).toEqual('0x0000000000000000000000000000000000000000')
     expect(CONSTANTS['ethereum'].MAXINT).toEqual('115792089237316195423570985008687907853269984665640564039457584007913129639935')
+    expect(CONSTANTS['ethereum'].WRAPPED).toEqual('0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2')
     
   });
 });
