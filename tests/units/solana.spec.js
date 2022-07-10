c46969771997d4f48e0122d8628f3434610f4c15
Sebastian Pape <s@pape.io>
2022-07-10 06:56:06 +0200

    fix native sol mint address

--- tests/units/solana.spec.js
+++ tests/units/solana.spec.js
@@ -7,7 +7,7 @@ describe('solana', () => {
     expect(CONSTANTS['solana'].ZERO).toEqual('0')
     expect(CONSTANTS['solana'].MAXINT).toEqual('18446744073709551615')
     expect(CONSTANTS['solana'].WRAPPED).toEqual('So11111111111111111111111111111111111111112')
-    expect(CONSTANTS['solana'].NATIVE).toEqual('SOL')
+    expect(CONSTANTS['solana'].NATIVE).toEqual('11111111111111111111111111111111')
     expect(CONSTANTS['solana'].DECIMALS).toEqual(9)
     expect(CONSTANTS['solana'].SYMBOL).toEqual('SOL')
     expect(CONSTANTS['solana'].CURRENCY).toEqual('Solana')
