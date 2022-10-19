19682d81e2b49b768e44b95bfae44a3e177817c7
Sebastian Pape <s@pape.io>
2022-10-19 11:18:33 +0200

    v6.3.1: fix exporting velas constants

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { CONSTANTS as ETHEREUM } from './ethereum'
 import { CONSTANTS as BSC } from './bsc'
 import { CONSTANTS as POLYGON } from './polygon'
 import { CONSTANTS as SOLANA } from './solana'
+import { CONSTANTS as VELAS } from './velas'
 
 let CONSTANTS = {}
 
@@ -9,5 +10,6 @@ CONSTANTS['ethereum'] = ETHEREUM
 CONSTANTS['bsc'] = BSC
 CONSTANTS['polygon'] = POLYGON
 CONSTANTS['solana'] = SOLANA
+CONSTANTS['velas'] = VELAS
 
 export { CONSTANTS }
